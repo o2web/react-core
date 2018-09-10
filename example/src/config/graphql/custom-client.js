590484ba29bb346d3459a5c0ae86a987c597aa5f
Guillaume Loubier <gloubier@o2web.ca>
2018-09-10 14:43:05 -0400

    Use env variable for client url

--- example/src/config/graphql/custom-client.js
+++ example/src/config/graphql/custom-client.js
@@ -6,7 +6,7 @@ const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:3001/graphql',
+    uri: process.env.REACT_APP_API_URL,
   }),
   cache,
 });
