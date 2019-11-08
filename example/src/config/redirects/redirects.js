8671b2b784ee3dfea0137d76106f8153d2102031
bedardo <obedard@o2web.ca>
2019-11-08 10:14:24 -0500

    Add parent routes to Redirector

--- example/src/config/redirects/redirects.js
+++ example/src/config/redirects/redirects.js
@@ -1,4 +1,7 @@
-// const redirects = [{ from: '/en/route-one', to: '/en/redirect-one' }];
+// const redirects = [
+// { from: '/en/route-one', to: '/en/redirect-one' }
+// { from: '/en/about/', to: '/en/artworks/', parent: true }
+// ];
 const redirects = [];
 
 export default redirects;
