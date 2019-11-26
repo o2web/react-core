c736fe107a8628a9070054f0c5b5feb3c2be858a
bedardo <obedard@o2web.ca>
2019-11-26 17:29:05 -0500

    Update SSR to use a custom store

--- example/server/index.js
+++ example/server/index.js
@@ -6,11 +6,13 @@ import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import enforce from 'express-sslify';
+import reduxThunk from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
 import cache from './cache';
 import ServerApp from '../src/app/components/ServerApp';
-import store from '../src/config/redux/store';
-import actions from '../src/app/actions/user';
+// import store from '../src/config/redux/store';
 import SSR_RESET_TYPE from '../src/config/redux/reset';
+import reducers from '../src/app/reducers';
 
 global.window = false;
 global.document = false;
@@ -19,47 +21,65 @@ const PORT = process.env.PORT || 80;
 const app = express();
 
 app.use('/static', express.static('build/static'));
+app.use('/build', express.static('build'));
+app.use('/robots.txt', express.static('build/static/robots.txt'));
+app.use('/sitemap.xml', express.static('build/static/sitemap.xml'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
 }
 
 app.get('*', cache(1), (request, response) => {
-  // reset redux store at every requests
-  store.dispatch({ type: SSR_RESET_TYPE });
-  store.dispatch(actions.validateNoToken());
+  // Create custom store
+  const middlewares = [reduxThunk];
+  const preloadedState = (typeof window !== 'undefined' && window.PRELOADED_STATE) ? window.PRELOADED_STATE : {};
+
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+  const customStore = createStoreWithMiddleware(reducers, preloadedState);
+  customStore.dispatch({ type: SSR_RESET_TYPE });
 
   renderToString(
-    <Provider store={store}>
-      <ServerApp request={request} response={response} initialRender />
+    <Provider store={customStore}>
+      <ServerApp request={request} response={response} customStore={customStore} initialRender />
     </Provider>,
   );
   // return if redirect
   if (response.headersSent) {
     return response.end();
   }
-
-  Promise.all(store.getState().promises.all)
+  Promise.all(customStore.getState().promises.all)
     .then(() => {
-      const content = renderToString(
-        <Provider store={store}>
-          <ServerApp request={request} response={response} />
+      renderToString(
+        <Provider store={customStore}>
+          <ServerApp
+            request={request}
+            response={response}
+            customStore={customStore}
+            initialRender
+          />
         </Provider>,
       );
+      Promise.all(customStore.getState().promises.all)
+        .then(() => {
+          const content = renderToString(
+            <Provider store={customStore}>
+              <ServerApp request={request} response={response} />
+            </Provider>,
+          );
+          const helmet = Helmet.renderStatic();
+          const state = JSON.stringify(customStore.getState()).replace(/</g, '\\u003c');
+          const bodyToReplace = '<div id="root"></div>';
+          const body = `<div id="root">${content}</div><script>window.PRELOADED_STATE = ${state}</script>`;
+          const headToReplace = '</head>';
+          const headEnd = `${helmet.title.toString()}${helmet.meta.toString()}${helmet.link.toString()}</head>`;
 
-      const helmet = Helmet.renderStatic();
-      const state = JSON.stringify(store.getState()).replace(/</g, '\\u003c');
-      const bodyToReplace = '<div id="root"></div>';
-      const body = `<div id="root">${content}</div><script>window.PRELOADED_STATE = ${state}</script>`;
-      const headToReplace = '</head>';
-      const headEnd = `${helmet.title.toString()}${helmet.meta.toString()}${helmet.link.toString()}</head>`;
-
-      const raw = fs
-        .readFileSync('./build/index.html', 'utf8')
-        .replace(bodyToReplace, body)
-        .replace(headToReplace, headEnd);
-      response.send(raw);
-      response.end();
+          const raw = fs
+            .readFileSync('./build/index.html', 'utf8')
+            .replace(bodyToReplace, body)
+            .replace(headToReplace, headEnd);
+          response.send(raw);
+          response.end();
+        });
     });
 });
 
--- example/src/app/components/ServerApp.js
+++ example/src/app/components/ServerApp.js
@@ -3,84 +3,76 @@ import PropTypes from 'prop-types';
 import I18n from 'redux-i18n';
 import { CookiesProvider } from 'react-cookie';
 import { StaticRouter, Route } from 'react-router-dom';
-import { BaseRoute, CacheBuster, GAListener, GTManager } from 'o2web-react-core';
+import { GAListener, GTManager } from 'o2web-react-core';
 
 import translations, {
   availableLanguages,
   defaultLanguage,
 } from '../../config/locales/translations';
-import PrimaryLayout from './layouts/PrimaryLayout';
-
-// styles
 import '../../assets/styles/app.scss';
+import PrimaryLayout from './layouts/PrimaryLayout';
 
 class ServerApp extends Component {
   static propTypes = {
     request: PropTypes.object.isRequired,
     response: PropTypes.object.isRequired,
     initialRender: PropTypes.bool,
+    customStore: PropTypes.object,
   };
 
   static defaultProps = {
     initialRender: false,
+    customStore: {},
   };
 
   getChildContext() {
-    const { response, initialRender } = this.props;
+    const { response, request, initialRender, customStore } = this.props;
     return {
       initialRender,
       translations,
       availableLanguages,
       defaultLanguage,
       response,
+      request,
+      customStore,
       ssr: true,
     };
   }
 
   render() {
     const { request } = this.props;
-
     return (
-      <CacheBuster>
-        {({ loading, isLatestVersion, refreshCacheAndReload }) => {
-          if (loading) return null;
-          if (!loading && !isLatestVersion) {
-            refreshCacheAndReload();
-          }
-
-          return (
-            <I18n translations={translations} initialLang={defaultLanguage}>
-              <CookiesProvider>
-                <StaticRouter location={request.url} context={{}}>
-                  <GAListener>
-                    <GTManager>
-                      <Route
-                        path="/"
-                        render={(props) => (
-                          <BaseRoute
-                            {...props}
-                            component={PrimaryLayout}
-                          />
-                        )}
-                      />
-                    </GTManager>
-                  </GAListener>
-                </StaticRouter>
-              </CookiesProvider>
-            </I18n>
-          );
-        }}
-      </CacheBuster>
+      <I18n translations={translations} initialLang={defaultLanguage}>
+        <CookiesProvider>
+          <StaticRouter location={request.url} context={{}}>
+            <GAListener>
+              <GTManager>
+                <Route
+                  path="/"
+                  render={(props) =>
+                    <Route
+                      {...props}
+                      component={PrimaryLayout}
+                    />
+                  }
+                />
+              </GTManager>
+            </GAListener>
+          </StaticRouter>
+        </CookiesProvider>
+      </I18n>
     );
   }
 }
 
 ServerApp.childContextTypes = {
   initialRender: PropTypes.bool,
+  customStore: PropTypes.object,
   translations: PropTypes.object.isRequired,
   availableLanguages: PropTypes.array.isRequired,
   defaultLanguage: PropTypes.string.isRequired,
   response: PropTypes.object.isRequired,
+  request: PropTypes.object.isRequired,
   ssr: PropTypes.bool.isRequired,
 };
 
